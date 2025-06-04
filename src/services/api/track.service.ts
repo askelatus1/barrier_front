f8faefeaa8590890195efc4230d8e4305f1cb8d5
Serenity <alter4444@gmail.com>
2025-06-04 04:16:54 +0400

    fix: исправить фильтрацию треков по идентификатору территории в TrackService

--- src/services/api/track.service.ts
+++ src/services/api/track.service.ts
@@ -90,7 +90,7 @@ export class TrackService {
   public getTracksByRegion(regionId: string): Observable<Track[]> {
     return this.tracks$.pipe(
       map(tracksMap => Array.from(tracksMap.values())
-        .filter(track => track.territory.id === regionId))
+        .filter(track => track.territoryId === regionId))
     );
   }
 
